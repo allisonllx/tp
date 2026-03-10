aa580696a0715c7bb04a770017befcb32ec2b085
Cxyu629 <chowxingyu123@gmail.com>
2026-03-10 19:12:59 +0800

    test: add test

---
+++ src/test/java/seedu/address/model/util/SampleDataUtilTest.java
@@ -0,0 +1,15 @@
+package seedu.address.model.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.contact.Contact;
+
+
+public class SampleDataUtilTest {
+    @Test
+    public void getSampleContacts() {
+        Contact[] contacts = SampleDataUtil.getSampleContacts();
+        assertTrue(contacts.length > 0);
+    }
+}
